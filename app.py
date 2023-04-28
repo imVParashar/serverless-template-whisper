d033e1766f6c72018ed1fb4fe1daa979f7a3d2bc
Vishal Parashar <87163900+imVParashar@users.noreply.github.com>
2023-04-29 01:20:57 +0530

    updated code for whisper large

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from io import BytesIO
 def init():
     global model
     
-    model = whisper.load_model("base")
+    model = whisper.load_model("large-v2")
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
@@ -19,15 +19,18 @@ def inference(model_inputs:dict) -> dict:
     # Parse out your arguments
     mp3BytesString = model_inputs.get('mp3BytesString', None)
     if mp3BytesString == None:
-        return {'message': "No input provided"}
+        return {'error': "No input provided", "status": 400}
     
     mp3Bytes = BytesIO(base64.b64decode(mp3BytesString.encode("ISO-8859-1")))
     with open('input.mp3','wb') as file:
         file.write(mp3Bytes.getbuffer())
     
     # Run the model
-    result = model.transcribe("input.mp3")
-    output = {"text":result["text"]}
+    options = dict(beam_size=5, best_of=5)
+    translate_options = dict(task="translate", **options)
+    result = model.transcribe("input.mp3", **translate_options, word_timestamps=True)
+    output = {"output": result, "status": 200}
+
     os.remove("input.mp3")
     # Return the results as a dictionary
     return output
--- download.py
+++ download.py
@@ -1,12 +1,11 @@
-# In this file, we define download_model
-# It runs during container build time to get model weights built into the container
+# This file runs during container build time to get model weights built into the container
 
-# In this example: A Huggingface BERT model
-
+# In this example: An OpenAI Whisper model
 import whisper
 import torch
 
 def download_model():
-    model = whisper.load_model("base")
+    model = whisper.load_model("large-v2")
+
 if __name__ == "__main__":
     download_model()
